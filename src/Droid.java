db517af50d7f740ac396c269818e9df9864904b4
Moth-Man <mothmanor@gmail.com>
2018-08-24 21:17:20 -0400

    Add damage variable in Droid class along with getter and setter.

--- src/Droid.java
+++ src/Droid.java
@@ -1,4 +1,47 @@
 
 public class Droid {
 
+	private String name;
+	private int health;
+	private int damage;
+	private int range;
+	private int x;
+	private int y;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public int getRange() {
+		return range;
+	}
+	public void setRange(int range) {
+		this.range = range;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	
 }
