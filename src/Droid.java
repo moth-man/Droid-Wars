bf9de8d17603246a2942ef9aa8829053c2ad627d
Moth-Man <mothmanor@gmail.com>
2018-08-24 22:10:08 -0400

    Modify Direction class, becomes Vector class. Make universal constructor for x and y variables in the Position class which is also created.

--- src/Droid.java
+++ src/Droid.java
@@ -5,8 +5,8 @@ public class Droid {
 	private int health;
 	private int damage;
 	private int range;
-	private int x;
-	private int y;
+	private Position position;
+	
 	public String getName() {
 		return name;
 	}
@@ -25,23 +25,21 @@ public class Droid {
 	public void setRange(int range) {
 		this.range = range;
 	}
-	public int getX() {
-		return x;
-	}
-	public void setX(int x) {
-		this.x = x;
-	}
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
 	public int getDamage() {
 		return damage;
 	}
 	public void setDamage(int damage) {
 		this.damage = damage;
-	}
 	
+	}
+	public void move(Board board, Vector vector){
+		
+		
+	}
+	public Position getPosition() {
+		return position;
+	}
+	public void setPosition(Position position) {
+		this.position = position;
+	}
 }
---
+++ src/Move.java
@@ -0,0 +1,13 @@
+
+public class Move {
+
+	private Position position;
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+}
